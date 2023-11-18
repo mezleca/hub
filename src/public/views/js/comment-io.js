c4252d47ed397d3bf8ab28b71eb1cf02b13e3cde
rel <92015390+mezleca@users.noreply.github.com>
2023-11-18 12:29:40 -0300

    update 0.8.8
    - quando o search nao consegue achar um user/post ele nao retorna nada
    - css da rota de search refeito
    - recommend section pronto
    - js de novo comentarios atualizado
    - pagina de media levemente modificada
    - fix altura do card de videos do usuario

--- src/public/views/js/comment-io.js
+++ src/public/views/js/comment-io.js
@@ -3,6 +3,7 @@ const comment_input = document.querySelector(".comment-input-v");
 const comments_div = document.querySelector(".comments");
 const vid_post_id = document.querySelector(".video-player");
 const piroca_len = document.querySelector(".piroca");
+const user_name = document.querySelector("#user_name");
 
 let last_comment = "";
 
@@ -25,21 +26,28 @@ const new_comment = async (content, id) => {
     const json = await response.json();
 
     if (!json.msg) {
-        window.location.reaload();
+        return window.location.reaload();
     }
     
     if (json.msg == "success") {
-        const new_p = document.createElement("p");
-        new_p.innerHTML = content;
 
-        console.log(Number(piroca_len.innerText))
+        const div = document.createElement("div");
+        const user = document.createElement("a");
+        const new_p = document.createElement("p");
+        
+        new_p.innerHTML = content;
+        user.href = `/profile/${user_name.innerText}`;
+        user.innerHTML = user_name.innerText;
+
+        div.appendChild(user);
+        div.appendChild(new_p);
 
         if (Number(piroca_len.innerText) == 0) {
             const no_comments = document.querySelector(".no_comments");
             comments_div.removeChild(no_comments);
-            piroca_len.innerHTML = (Number(piroca_len.innerText) + 1);
+            piroca_len.innerHTML = Number(piroca_len.innerText) + 1;
         }
-        comments_div.appendChild(new_p);
+        comments_div.appendChild(div);
     }
     else {
         console.error(json);
